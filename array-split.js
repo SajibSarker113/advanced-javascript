d47337cecf3e4af04aaffc584783b1e83b18ecf9
SajibSarker113 <66987905+SajibSarker113@users.noreply.github.com>
2020-08-08 13:07:57 +0600

    array split

---
+++ array-split.js
@@ -0,0 +1,18 @@
+/*
+const nums = [1,2,3,4,5,6,7,8];
+const part = nums.slice(2,5);
+console.log(part);
+
+const nums = [1,2,3,4,5,6,7,8];
+    // 2 theke poroborti 3 kete anbe and baki numgulo duke jabe
+const removed = nums.splice(2,3, 77,88,99);
+console.log(removed);
+console.log(nums);
+*/
+
+const nums = [1,2,3,4,5,6,7,8];
+const together = nums.join("");
+ const together = nums.join(".,");
+const together = nums.join("-any--");
+
+console.log(together);
